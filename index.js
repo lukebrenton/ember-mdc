38eed79d29696c50e4f60283ad7a359c8dadbd50
Aust <austp17@gmail.com>
2017-05-30 16:16:58 -0600

    Make scss importing work

--- index.js
+++ index.js
@@ -6,33 +6,191 @@
 var mergeTrees = require('broccoli-merge-trees');
 var Funnel = require('broccoli-funnel');
 var materialPackages = [
-  { css: true, js: true, name: 'animation', path: 'animation' },
-  { css: false, js: true, name: 'autoInit', path: 'auto-init' },
-  { css: false, js: true, name: 'base', path: 'base' },
-  { css: true, js: false, name: 'button', path: 'button' },
-  { css: true, js: false, name: 'card', path: 'card' },
-  { css: true, js: true, name: 'checkbox', path: 'checkbox' },
-  { css: true, js: true, name: 'dialog', path: 'dialog' },
-  { css: true, js: true, name: 'drawer', path: 'drawer' },
-  { css: true, js: false, name: 'elevation', path: 'elevation' },
-  { css: true, js: false, name: 'fab', path: 'fab' },
-  { css: true, js: true, name: 'formField', path: 'form-field' },
-  { css: true, js: true, name: 'gridList', path: 'grid-list' },
-  { css: true, js: true, name: 'iconToggle', path: 'icon-toggle' },
-  { css: true, js: false, name: 'layoutGrid', path: 'layout-grid' },
-  { css: true, js: false, name: 'list', path: 'list' },
-  { css: true, js: true, name: 'menu', path: 'menu' },
-  { css: true, js: true, name: 'radio', path: 'radio' },
-  { css: true, js: true, name: 'ripple', path: 'ripple' },
-  { css: false, js: false, name: 'rtl', path: 'rtl' },
-  { css: true, js: true, name: 'select', path: 'select' },
-  { css: true, js: true, name: 'snackbar', path: 'snackbar' },
-  { css: true, js: false, name: 'switch', path: 'switch' },
-  { css: true, js: true, name: 'tabs', path: 'tabs' },
-  { css: true, js: true, name: 'textfield', path: 'textfield' },
-  { css: true, js: false, name: 'theme', path: 'theme' },
-  { css: true, js: true, name: 'toolbar', path: 'toolbar' },
-  { css: true, js: false, name: 'typography', path: 'typography' }
+  {
+    css: true,
+    js: true,
+    name: 'animation',
+    path: 'animation'
+  },
+  {
+    css: false,
+    js: true,
+    name: 'autoInit',
+    path: 'auto-init'
+  },
+  {
+    css: false,
+    js: true,
+    name: 'base',
+    path: 'base'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'ripple', 'theme', 'typography'],
+    js: false,
+    name: 'button',
+    path: 'button'
+  },
+  {
+    css: true,
+    depends: ['elevation', 'theme', 'typography', 'rtl'],
+    js: false,
+    name: 'card',
+    path: 'card'
+  },
+  {
+    css: true,
+    depends: ['animation', 'ripple', 'rtl'],
+    js: true,
+    name: 'checkbox',
+    path: 'checkbox'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'dialog',
+    path: 'dialog'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'rtl', 'theme'],
+    js: true,
+    name: 'drawer',
+    path: 'drawer',
+    structure: ['permanent', 'persistent', 'slidable', 'temporary']
+  },
+  {
+    css: true,
+    depends: ['animation'],
+    js: false,
+    name: 'elevation',
+    path: 'elevation'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'ripple', 'theme'],
+    js: false,
+    name: 'fab',
+    path: 'fab'
+  },
+  {
+    css: true,
+    depends: ['rtl', 'theme', 'typography'],
+    js: true,
+    name: 'formField',
+    path: 'form-field'
+  },
+  {
+    css: true,
+    depends: ['rtl', 'theme', 'typography'],
+    js: true,
+    name: 'gridList',
+    path: 'grid-list'
+  },
+  {
+    css: true,
+    depends: ['animation', 'ripple', 'theme'],
+    js: true,
+    name: 'iconToggle',
+    path: 'icon-toggle'
+  },
+  {
+    css: true,
+    js: false,
+    name: 'layoutGrid',
+    path: 'layout-grid'
+  },
+  {
+    css: true,
+    depends: ['ripple', 'rtl', 'theme', 'typography'],
+    js: false,
+    name: 'list',
+    path: 'list'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'theme', 'typography'],
+    js: true,
+    name: 'menu',
+    path: 'menu',
+    structure: ['simple']
+  },
+  {
+    css: true,
+    depends: ['animation', 'ripple', 'theme'],
+    js: true,
+    name: 'radio',
+    path: 'radio'
+  },
+  {
+    css: true,
+    depends: ['animation'],
+    js: true,
+    name: 'ripple',
+    path: 'ripple'
+  },
+  {
+    css: false,
+    js: false,
+    name: 'rtl',
+    path: 'rtl'
+  },
+  {
+    css: true,
+    depends: ['animation', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'select',
+    path: 'select'
+  },
+  {
+    css: true,
+    depends: ['animation', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'snackbar',
+    path: 'snackbar'
+  },
+  {
+    css: true,
+    depends: ['animation', 'elevation', 'theme'],
+    js: false,
+    name: 'switch',
+    path: 'switch'
+  },
+  {
+    css: true,
+    depends: ['animation', 'ripple', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'tabs',
+    path: 'tabs',
+    structure: ['tab-bar', 'tab']
+  },
+  {
+    css: true,
+    depends: ['animation', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'textfield',
+    path: 'textfield'
+  },
+  {
+    css: true,
+    js: false,
+    name: 'theme',
+    path: 'theme'
+  },
+  {
+    css: true,
+    depends: ['elevation', 'rtl', 'theme', 'typography'],
+    js: true,
+    name: 'toolbar',
+    path: 'toolbar'
+  },
+  {
+    css: true,
+    js: false,
+    name: 'typography',
+    path: 'typography'
+  }
 ];
 
 module.exports = {
@@ -47,17 +205,12 @@ module.exports = {
     materialPackages.forEach(function(pkg) {
       if (pkg.css) {
         app.import(`vendor/@material/${pkg.path}/dist/mdc.${pkg.path}.min.css`);
-        app.import(`vendor/@material/${pkg.path}/mdc-${pkg.path}.scss`);
       }
 
       if (pkg.js) {
         app.import(`vendor/@material/${pkg.path}/dist/mdc.${pkg.name}.min.js`, {
           using: [{ as: `@material/${pkg.path}`, transformation: 'amd' }]
         });
-      }
-
-      if (pkg.name == 'rtl') {
-        app.import(`vendor/@material/${pkg.path}/_mixins.scss`);
       }
     });
   },
@@ -71,6 +224,27 @@ module.exports = {
    * anything directly from a `node_modules/` folder.
    */
   treeForVendor: function() {
+    function getDependencyTree(path, depends) {
+      var trees = depends.map(function(dependency) {
+        var pkg = materialPackages.filter(function(pkg) {
+          return pkg.name == dependency;
+        })[0];
+
+        var tree = new Funnel('node_modules/@material', {
+          destDir: `${path}/@material`,
+          include: ['*/*.scss']
+        });
+
+        if (pkg.depends) {
+          return mergeTrees([tree, getDependencyTree(`${path}/@material/${pkg.path}`, pkg.depends)], { overwrite: true });
+        }
+
+        return tree;
+      });
+
+      return mergeTrees(trees, { overwrite: true });
+    }
+
     var trees = materialPackages.map(function(pkg) {
       var include = [];
       if (pkg.css) {
@@ -86,11 +260,39 @@ module.exports = {
         include.push('_mixins.scss');
       }
 
-      return new Funnel(`node_modules/@material/${pkg.path}`, {
+      var tree = new Funnel(`node_modules/@material/${pkg.path}`, {
         destDir: `@material/${pkg.path}`,
         include: include
       });
+
+      if (pkg.structure) {
+        var trees = [tree];
+
+        pkg.structure.forEach(function(dir) {
+          trees.push(new Funnel(`node_modules/@material/${pkg.path}/${dir}`, {
+            destDir: `@material/${pkg.path}/${dir}`,
+            include: ['*.scss', '*.css'] // include *.css for drawer/slidable/*.css
+          }));
+
+          if (pkg.depends) {
+            trees.push(getDependencyTree(`@material/${pkg.path}/${dir}`, pkg.depends));
+          }
+        });
+
+        tree = mergeTrees(trees, { overwrite: true });
+      }
+
+      if (pkg.depends) {
+        return mergeTrees([tree, getDependencyTree(`@material/${pkg.path}`, pkg.depends)], { overwrite: true });
+      }
+
+      return tree;
     });
+
+    trees.push(new Funnel('node_modules/ember-mdc', {
+      destDir: 'material-components-web',
+      include: ['material-components-web.scss']
+    }));
 
     return this._super(mergeTrees(trees, { overwrite: true }));
   }
